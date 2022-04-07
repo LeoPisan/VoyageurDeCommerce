df81a1191617c81170b449a40e232cac89c82fed
LeoPisan <elia.gautier.2004@gmail.com>
2022-04-07 15:25:45 +0200

    ajout de la description de Outils.UsineDepart()

--- VoyageurDeCommerce/modele/algorithmes/Outils.cs
+++ VoyageurDeCommerce/modele/algorithmes/Outils.cs
@@ -9,15 +9,25 @@ namespace VoyageurDeCommerce.modele.algorithmes
 {
     class Outils
     {
+        /// <summary>
+        /// cherche l'usine d'une liste de lieux
+        /// </summary>
+        /// <param name="liste">liste dans laquelle l'usine doit être cherchée</param>
+        /// <returns>Lieu représentant l'usine</returns>
         public static Lieu UsineDepart(List<Lieu> liste)
         {
-            Lieu retour = liste[0];
+            Lieu retour = null;
             foreach (Lieu l in liste)
             {
                 if (l.Type == TypeLieu.USINE)
                     retour = l;
             }
             return retour;
+        }
+
+        public List<Lieu> InverseElements(int a, int b)
+        {
+
         }
     }
 }
