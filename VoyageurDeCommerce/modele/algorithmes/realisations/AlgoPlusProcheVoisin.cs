31fc5bda37246f0969cc209b09f8c8d139434ecb
LeoPisan <92891905+LeoPisan@users.noreply.github.com>
2022-04-07 10:54:27 +0200

    ajout de AlgoPlusProcheVoisin.MagasinProche()

--- VoyageurDeCommerce/modele/algorithmes/realisations/AlgoPlusProcheVoisin.cs
+++ VoyageurDeCommerce/modele/algorithmes/realisations/AlgoPlusProcheVoisin.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VoyageurDeCommerce.modele.lieux;
+using VoyageurDeCommerce.modele.distances;
 
 namespace VoyageurDeCommerce.modele.algorithmes.realisations
 {
     class AlgoPlusProcheVoisin : Algorithme
     {
+        private Lieu dernierVisite;
+
         public override string Nom => "Plus proche voisin";
 
         public override void Executer(List<Lieu> listeLieux, List<Route> listeRoute)
         {
-            throw new NotImplementedException();
+            
+        }
+
+        private Lieu MagasinProche()
+        {
+            int min = FloydWarshall.Distance(dernierVisite, this.Tournee.ListeLieux[0]);
+            Lieu retour = this.Tournee.ListeLieux[0];
+            foreach (Lieu l in this.Tournee.ListeLieux)
+            {
+                if (FloydWarshall.Distance(dernierVisite, l) <= min)
+                {
+                    retour = l;
+                }
+            }
+            return retour;
         }
     }
 }
