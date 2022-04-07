9f6ba30dfdccdc8513814673ac3fad535057c8d5
LeoPisan <92891905+LeoPisan@users.noreply.github.com>
2022-04-07 10:35:47 +0200

    Ajout de l'algorithme du parcours par le plus proche voisin

---
+++ VoyageurDeCommerce/modele/algorithmes/realisations/AlgoPlusProcheVoisin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoyageurDeCommerce.modele.lieux;
+
+namespace VoyageurDeCommerce.modele.algorithmes.realisations
+{
+    class AlgoPlusProcheVoisin : Algorithme
+    {
+        public override string Nom => "Plus proche voisin";
+
+        public override void Executer(List<Lieu> listeLieux, List<Route> listeRoute)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
