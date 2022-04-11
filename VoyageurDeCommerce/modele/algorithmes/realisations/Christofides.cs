25c64656ede1b555830e87a69ee43573f528f092
Evan Damas <evan.damas.2003@gmail.com>
2022-04-11 12:00:21 +0200

    Ajout de l'algortihme de Chhristofides

---
+++ VoyageurDeCommerce/modele/algorithmes/realisations/Christofides.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using VoyageurDeCommerce.modele.distances;
+using VoyageurDeCommerce.modele.lieux;
+
+namespace VoyageurDeCommerce.modele.algorithmes.realisations
+{
+    /// <summary>
+    /// Exemple de classe Algorithme, à ne pas garder
+    /// </summary>
+    public class Christofides : Algorithme
+    {
+
+        public override string Nom => "Christofides";
+
+        public override void Executer(List<Lieu> listeLieux, List<Route> listeRoute)
+        {
+            //Lancer FloydWarshall
+            FloydWarshall.calculerDistances(listeLieux, listeRoute);
+
+
+        }
+    }
+}
