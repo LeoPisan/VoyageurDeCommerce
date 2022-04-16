73c1cdbb7ea0fd6c4169a048bf90014b8a53b408
LeoPisan <92891905+LeoPisan@users.noreply.github.com>
2022-04-16 21:24:12 +0200

    Ajout de la classe abstraite des algorithmes génétiques

---
+++ VoyageurDeCommerce/modele/algorithmes/realisations/AlgoGenetique.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoyageurDeCommerce.modele.lieux;
+
+namespace VoyageurDeCommerce.modele.algorithmes.realisations
+{
+    abstract class AlgoGenetique : Algorithme
+    {
+
+    }
+}
