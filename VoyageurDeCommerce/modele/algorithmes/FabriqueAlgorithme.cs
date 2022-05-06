b7c1aaf4c2ab522b0f7b2429f13bf6f947c1b568
Evan Damas <evan.damas.2003@gmail.com>
2022-05-07 00:19:39 +0200

    Séparation de christofides en 2 versions

--- VoyageurDeCommerce/modele/algorithmes/FabriqueAlgorithme.cs
+++ VoyageurDeCommerce/modele/algorithmes/FabriqueAlgorithme.cs
@@ -20,7 +20,7 @@ namespace VoyageurDeCommerce.modele.algorithmes
             Algorithme algo;
             switch (type)
             {
-                case TypeAlgorithme.ALGOEXEMPLE: algo = new AlgoExemple(); break;
+                //case TypeAlgorithme.ALGOEXEMPLE: algo = new AlgoExemple(); break;
                 case TypeAlgorithme.CROISSANT: algo = new AlgorithmeCroissant(); break;
                 case TypeAlgorithme.INSERTION_AU_PLUS_PROCHE: algo = new InsertionAuPlusProche(); break;
                 case TypeAlgorithme.PLUSPROCHEVOISIN: algo = new AlgoPlusProcheVoisin(); break;
@@ -28,6 +28,7 @@ namespace VoyageurDeCommerce.modele.algorithmes
                 case TypeAlgorithme.VOISINAGE_TOURNEE: algo = new AlgoVoisinageTournee(); break; 
                 case TypeAlgorithme.CHRISTOFIDES: algo = new Christofides(); break;
                 case TypeAlgorithme.GENETIQUE_ROULETTE: algo = new AlgoGenetiqueRoulette(); break;
+                case TypeAlgorithme.CHRISTOFIDES_VERSION_BRUTE: algo = new Christofides_VersionBrute(); break;
 
                 default: throw new ExceptionAlgorithme("Vous n'avez pas modifié la fabrique des algorithmes !");
             }
--- VoyageurDeCommerce/modele/algorithmes/TypeAlgorithme.cs
+++ VoyageurDeCommerce/modele/algorithmes/TypeAlgorithme.cs
@@ -4,12 +4,13 @@
     public enum TypeAlgorithme
     {
         ALGOEXEMPLE,
-        CROISSANT,
-        INSERTION_AU_PLUS_PROCHE,
-        PLUSPROCHEVOISIN,
-        INSERTION_AU_PLUS_LOIN,
-        VOISINAGE_TOURNEE,
-        CHRISTOFIDES,
-        GENETIQUE_ROULETTE
+        CROISSANT,                  // Elia
+        INSERTION_AU_PLUS_PROCHE,   // Evan
+        PLUSPROCHEVOISIN,           // Elia
+        INSERTION_AU_PLUS_LOIN,     // Evan
+        VOISINAGE_TOURNEE,          // ELia
+        CHRISTOFIDES,               // Evan
+        GENETIQUE_ROULETTE,         // Elia
+        CHRISTOFIDES_VERSION_BRUTE  // Evan
     }
 }
